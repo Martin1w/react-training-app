93a772aae5242a59dc7dde028e74f5a4eb3e1f68
Marcin <marcin.walczakdev@gmail.com>
2023-03-12 19:45:11 +0100

    added NewExpenses and useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import NewExpense from './components/NewExpense/NewExpense'
 import Expenses from './components/Expenses/Expenses'
 
 const App = () => {
@@ -24,18 +25,18 @@ const App = () => {
 		},
 	]
 
-// return React.createElement(
-// 	'div',
-// 	{},
-// 	React.createElement('h2', {}, "Let's get started!"),
-// 	React.createElement(Expenses, {items:expenses}),
+	// return React.createElement(
+	// 	'div',
+	// 	{},
+	// 	React.createElement('h2', {}, "Let's get started!"),
+	// 	React.createElement(Expenses, {items:expenses}),
 
-// )
+	// )
 
 	return (
 		<div>
-			<h2>Let's get started!</h2>
-			<Expenses items={expenses} /> 
+			<NewExpense />
+			<Expenses items={expenses} />
 		</div>
 	)
 }
