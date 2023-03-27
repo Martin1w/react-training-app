42aea36a7ded9cac169284596913ac70b251dfd4
Marcin <marcin.walczakdev@gmail.com>
2023-03-27 14:11:49 +0200

    added conditional content and filtered expenses

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const DUMMY_EXPENSES = [
 		amount: 94.12,
 		date: new Date(2023, 7, 14),
 	},
-	{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+	{ id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2023, 2, 12) },
 	{
 		id: 'e3',
 		title: 'Car Insurance',
@@ -33,7 +33,7 @@ const App = () => {
 		setExpenses(prevExpenses => {
 			return [expense, ...prevExpenses]
 		})
-		console.log(expense); 
+		console.log(expense)
 	}
 
 	return (
