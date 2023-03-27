f6ca89c884d2ab6683e17f8ac4da322501d5583a
Marcin <marcin.walczakdev@gmail.com>
2023-03-27 15:00:18 +0200

    changed Expenses.css

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,18 +15,20 @@ const Expenses = props => {
 		return expense.date.getFullYear().toString() === filteredYear
 	})
 
+	let expensesContent = <p>No expenses found</p>
+	if (filteredExpenses.length > 0) {
+		expensesContent =
+			filteredExpenses.length > 0 &&
+			filteredExpenses.map(expense => (
+				<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+			))
+	}
+
 	return (
 		<div>
 			<Card className='expenses'>
 				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-				{filteredExpenses.length === 0 ? (
-					<p>No expenses found</p>
-				) : (
-					filteredExpenses.map(expense => (
-						<ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
-					))
-				)}
+				{expensesContent}
 
 				{/* <ExpenseItem
 					title={props.items[0].title}
